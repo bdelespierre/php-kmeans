765465d6f1c40bcb193aff13ee2c78b5626affa0
Benjamin Delespierre <benjamin.delespierre@gmail.com>
2022-05-04 00:04:49 +0200

    adding haversine and GPS centroid formulas to Math helper

--- src/Math.php
+++ src/Math.php
@@ -65,4 +65,62 @@ class Math
 
         return [$z0, $z1];
     }
+
+    public static int $earthRadius = 6371000;
+
+    /**
+     * Calculates the great-circle distance between two points, with
+     * the Haversine formula.
+     *
+     * @see https://stackoverflow.com/a/14751773/17403258
+     *
+     * @param array{0: float, 1: float} $from
+     * @param array{0: float, 1: float} $to
+     * @return float
+     */
+    public static function haversine($from, $to): float
+    {
+        return 2 * self::$earthRadius * asin(sqrt(
+            pow(sin(deg2rad($to[0] - $from[0]) / 2), 2)
+            + cos(deg2rad($from[0])) * cos(deg2rad($to[0]))
+            * pow(sin(deg2rad($to[1] - $from[1]) / 2), 2)
+        ));
+    }
+
+    /**
+     * Calculates the centroid of GPS coordinates
+     *
+     * @see https://stackoverflow.com/questions/6671183
+     *
+     * @param array<array{0: float, 1: float}> $points
+     * @return array{0: float, 1: float}
+     */
+    public static function gpsCentroid(array $points): array
+    {
+        if (count($points) == 1) {
+            return $points[0];
+        }
+
+        $x = $y = $z = 0;
+
+        foreach ($points as $point) {
+            $lat = deg2rad($point[0]);
+            $long = deg2rad($point[0]);
+
+            $x += cos($lat) * cos($long);
+            $y += cos($lat) * sin($long);
+            $z += sin($lat);
+        }
+
+        $x /= count($points);
+        $y /= count($points);
+        $z /= count($points);
+
+        $hypotenuse = sqrt(pow($x, 2) + pow($y, 2));
+
+        $long = atan2($y, $x);
+        $lat = atan2($z, $hypotenuse);
+
+        return [rad2deg($lat), rad2deg($long)];
+    }
 }
